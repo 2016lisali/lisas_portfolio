3125a457ba6f8ccd83224de5aefd6854bd35c20a
Lisa <2016lisali@gmail.com>
2024-08-14 11:55:22 +1000

    Chore: Update skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,6 +3,9 @@ import { Animated } from "react-animated-css";
 import { Container, Col, Row } from "react-bootstrap";
 
 const skills = [
+    { href: "#nextjs_", title: "Next.js" },
+    { href: "#crm_", title: "CRM" },
+    { href: "#tailwind_", title: "TAILWIND" },
     { href: "#git_", title: "GIT" },
     { href: "#json_", title: "JSON" },
     { href: "#css_", title: "CSS" },
@@ -69,11 +72,11 @@ const Skills = () => {
                 <Row className="align-items-center">
                     <Col md className="text-container fs-5 px-4">
                         <h4>Languages</h4>
-                        <p>Python, Javascript, TypeScript</p>
+                        <p>Javascript, TypeScript, Python</p>
                         <h4>Front-End</h4>
                         <p>
-                            React, Redux, Angular, MUI,
-                            Bootstrap, HTML, CSS, Sass, Responsive Design ...
+                            Next.js, React, Redux, Angular, Tailwind, Bootstrap,
+                            HTML, CSS, Sass, Responsive Design ...
                         </p>
                         <h4>Back-End</h4>
                         <p>Node.js, Express.js, RestAPI, MySQL, MongoDB ...</p>
