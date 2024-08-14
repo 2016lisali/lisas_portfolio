378f17658bddfe44da62b0b5a7f45a7d7b1e8fb5
Lisa <2016lisali@gmail.com>
2024-08-14 11:53:23 +1000

    chore: Use dynamic year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 const Footer = () => {
+    const curYear = new Date().getFullYear();
     return (
         <div className="border border-top small text-center text-secondary py-2">
-            <p className="my-0">© 2022 Lisa Li. All Rights Reserved.</p>
+            <p className="my-0">© {curYear} Lisa Li. All Rights Reserved.</p>
         </div>
     );
 };
